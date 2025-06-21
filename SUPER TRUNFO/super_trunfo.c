c3519686c80285a93beca9fc0da3173a23c98ab9
Carlawe <freitaswalle05@gmail.com>
2025-06-21 15:55:13 +0000

    COMPARAÇÃO

--- SUPER TRUNFO/super_trunfo.c
+++ SUPER TRUNFO/super_trunfo.c
@@ -1,5 +1,6 @@
 
-#include <stdio.h>
+     
+ #include <stdio.h>
 int main() { 
         //Declaração das variaveis
      char pais1[20], pais2[20];
@@ -12,14 +13,11 @@ int main() {
      float superpoder1, superpoder2;
      int resultado1, resultado2;
      int atributo1, atributo2;
-     float soma1, soma2;
+     int soma;
 
     
    
-    
-   
-   
-   //Entrada dos dados da primeira carta.
+    //Entrada dos dados da primeira carta.
      printf("Bem vindo ao super trunfo ***Países***!!!!!\n");
      printf("Faça o cadastro da carta 1 : \n");
      printf("País\n");
@@ -93,7 +91,7 @@ int main() {
             
                               
           
-                 printf("Carta 1 %s, Carta 2 %s\n", pais1 , pais2);
+                 printf("Carta 1 %s, Carta 2 %s\n", pais1 , pais2);//Saída dos nomes dos paises de cada carta
                
                
                               //Comparacao das cartas por atributo
@@ -101,46 +99,41 @@ int main() {
             {
            case 1:
              printf("Você escolheu a opção população!\n");
-             printf("Populacão Carta 1: %d - População carta 2: %d\n", populacao1, populacao2);
-             
-             resultado1 = populacao1 > populacao2 ? 1 : 0;
-             
+             printf("Populacão Carta 1: %d - População carta 2: %d\n", populacao1, populacao2); //Saída valor atributo 1 e atributo 2.
+             //resultado1 = populacao1 > populacao2 ? 1 : 0;
+             resultado1 = (float)populacao1 + area1; (float)populacao1 + pib1; populacao1 + pontoturistico1; populacao1 + (float)densidadepopulacional1; //Resultado da soma dos atributos carta 1 + carta2.
              break;   
              
          
             case 2:
             printf("Você escolheu a opção Área!\n");
-            printf("Área Carta 1: %.2f - Área carta 2: %.2f\n", area1, area2);
-            
-           
+            printf("Área Carta 1: %.2f - Área carta 2: %.2f\n", area1, area2);//Saída valor atributo 1 e atributo 2.
+             //resultado1 = area1 > area2 ? 1 : 0;
+             resultado1 =  area1 + (float)populacao1 ; area1 + pib1; area1 + (float)pontoturistico1; area1 + densidadepopulacional1;//Resultado da soma dos atributos carta 1 + carta2.
              break;
                  
            case 3:
              printf("Você escolheu a opção Pib!\n");
-             printf("Pib Carta 1: %.2f - Pib carta 2: %.2f\n", pib1, pib2);
-              
-             resultado1 = pib1 > pib2 ? 1 : 0;
-            
+             printf("Pib Carta 1: %.2f - Pib carta 2: %.2f\n", pib1, pib2);//Saída valor atributo 1 e atributo 2.
+             //resultado1 = pib1 > pib2 ? 1 : 0;
+             resultado1 = pib1 + (float)populacao1 ; pib1 + area1;  pib1 + (float)pontoturistico1; pib1 + densidadepopulacional1;//Resultado da soma dos atributos carta 1 + carta2.
             break;
                  
            case 4:
             printf("Você escolheu a opção Ponto turístico!\n");
-            printf("Ponto turístico Carta 1: %d - Ponto turistico carta 2: %d\n", pontoturistico1, pontoturistico2);
-             
-            resultado1 = pontoturistico1 > pontoturistico2 ? 1 : 0;
-            
-           break;
+            printf("Ponto turístico Carta 1: %d - Ponto turistico carta 2: %d\n", pontoturistico1, pontoturistico2);//Saída valor atributo 1 e atributo 2.
+            //resultado1 = pontoturistico1 > pontoturistico2 ? 1 : 0;
+            resultado1 = pontoturistico1 + populacao1; (float)pontoturistico1 + area1; (float)pontoturistico1 +  pib1;  (float)pontoturistico1 + densidadepopulacional1;//Resultado da soma dos atributos carta 1 + carta2.
                  
            case 5:
-                            //aparece 0 nos dois resuLtados NÃO OBEDECEM AO COMANDO
+                         
            printf("Você escolheu a opção Densidade populacional!\n");
-           printf("Densidade populacional Carta 1: %.2f - Densidade populacional carta 2: %.2f\n", densidadepopulacional1, densidadepopulacional2);
-            
-            resultado1 = densidadepopulacional1 < densidadepopulacional2 ? 1 : 0;
+           printf("Densidade populacional Carta 1: %f - Densidade populacional carta 2: %f\n", densidadepopulacional1, densidadepopulacional2);//Saída valor atributo 1 e atributo 2.
+          //resultado1 = densidadepopulacional1 < densidadepopulacional2 ? 1 : 0;
+          resultado1 = densidadepopulacional1 + (float)populacao1; densidadepopulacional1 + area1; densidadepopulacional1 + pib1; densidadepopulacional1 + (float)pontoturistico1;//Resultado da soma dos atributos carta 1 + carta2.
+          break; 
            
-            break; 
-           
-            default:
+            default:                             // Opção do segundo atributo.
                printf("Opção de jogo inválida!");
            }
               
@@ -163,46 +156,41 @@ int main() {
                   switch(atributo2){
                  case 1:
              printf("Você escolheu a opção população!\n");
-             printf("Populacão Carta 1: %d - População carta 2: %d\n", populacao1, populacao2);
-            
-             resultado2 = populacao1 > populacao2 ? 1 : 0;
-             
+             printf("Populacão Carta 1: %d - População carta 2: %d\n", populacao1, populacao2);//Saída valor atributo 1 e atributo 2.
+             //resultado2 = populacao1 > populacao2 ? 1 : 0;
+             resultado2 = populacao2 + area2; populacao2 + (float)pib2; populacao2 + pontoturistico2; (float)populacao2 + densidadepopulacional2;//Resultado da soma dos atributos carta 1 + carta2.
            
              break;   
              
          
             case 2:
             printf("Você escolheu a opção Área!\n");
-             printf("Área Carta 1: %.2f - Área carta 2: %.2f\n", area1, area2);
-             
-             resultado2 = area1 > area2 ? 1 : 0;
-             
+             printf("Área Carta 1: %.2f - Área carta 2: %.2f\n", area1, area2);//Saída valor atributo 1 e atributo 2.
+             //resultado2 = area1 > area2 ? 1 : 0;
+             resultado2 = area2 + (float)populacao2; area2 + pib2; area2 + (float)pontoturistico2; area2 + densidadepopulacional2;//Resultado da soma dos atributos carta 1 + carta2.
              break;
                  
            case 3:
              printf("Você escolheu a opção Pib!\n");
-             printf("Pib Carta 1: %.2f - Pib carta 2: %.2f\n", pib1, pib2);
-              
-             resultado2 = pib1 > pib2 ? 1 : 0;
-            
+             printf("Pib Carta 1: %.2f - Pib carta 2: %.2f\n", pib1, pib2);//Saída valor atributo 1 e atributo 2.
+             //resultado2 = pib1 > pib2 ? 1 : 0;
+             resultado2 = pib2 + (float)populacao2; pib2 + area2;  pib2 + (float)pontoturistico2; pib2 + densidadepopulacional2; //Resultado da soma dos atributos carta 1 + carta2.
              
             break;
                  
            case 4:
             printf("Você escolheu a opção Ponto turístico!\n");
-            printf("Ponto turistico Carta 1: %d - Ponto turistico carta 2: %d\n", pontoturistico1, pontoturistico2);
-             
-             resultado2 = pontoturistico1 > pontoturistico2 ? 1 : 0;
-            
+            printf("Ponto turistico Carta 1: %d - Ponto turistico carta 2: %d\n", pontoturistico1, pontoturistico2);//Saída valor atributo 1 e atributo 2.
+            // resultado2 = pontoturistico1 > pontoturistico2 ? 1 : 0;
+             resultado2 = pontoturistico2 + populacao2; (float)pontoturistico2 + area2; (float)pontoturistico2 +  pib2;  (float)pontoturistico2 + densidadepopulacional2;//Resultado da soma dos atributos carta 1 + carta2.
             break;   
            
             case 5:
-                            //aparece 0 nos dois resuLtados NÃO OBEDECEM AO COMANDO
+                            
              printf("Você escolheu a opção Densidade populscional!\n");
-             printf("Densidade populacional Carta 1: %.2f - Densidade populacional carta 2: %.2f\n", densidadepopulacional1, densidadepopulacional2);
-               
-             resultado2 = densidadepopulacional1 < densidadepopulacional2 ? 1 : 0;
-           
+             printf("Densidade populacional Carta 1: %f - Densidade populacional carta 2: %f\n", densidadepopulacional1, densidadepopulacional2);//Saída valor atributo 1 e atributo 2.
+             //resultado2 = densidadepopulacional1 < densidadepopulacional2 ? 1 : 0;
+             resultado2 = densidadepopulacional2 + (float)populacao2; densidadepopulacional2 + area2; densidadepopulacional2 + pib2; densidadepopulacional2 + (float)pontoturistico2;//Resultado da soma dos atributos carta 1 + carta2.
             break; 
            
             default:
@@ -211,22 +199,20 @@ int main() {
            }
            
            printf("\n");
-
-          if(resultado1 && resultado2){
+                               //Comparação das cartas,
+          if(resultado1 > resultado2){
             printf("Carta 1 venceu\n");
-          }else if(resultado1 != resultado2){
+          }else if(resultado1 == resultado2){
             printf("Empatou!!");
 
-          }else 
-             printf("Carta 2 venceu!\n");
+          }else{
+            printf("Carta 2 venceu!\n");
           }
-          
-         
-        
+        }
             printf("\n");
             
-            printf( "resultado Carta 1 %.2f, Resultado carta 2 %.2f\n", soma1, soma2);
+         printf( "resultado Carta 1: %d, Resultado carta 2: %d", resultado1, resultado2);   //saida da soma dos resultados carta 1 + carta 2.
             
            
             return 0;
-        }
+        }   
